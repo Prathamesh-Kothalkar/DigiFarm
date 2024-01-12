7cd643e757c73a90250439815d76197289347ae0
Prathamesh <prathameshkothalkar9021@gmail.com>
2024-01-12 18:52:22 +0530

    v 0.4

--- app/src/main/java/com/example/digifarm/MainActivity.java
+++ app/src/main/java/com/example/digifarm/MainActivity.java
@@ -2,14 +2,46 @@
 package com.example.digifarm;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.example.digifarm.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            int id=item.getItemId();
+            if(id==R.id.home){
+                replaceFragment(new HomeFragment());
+            }
+            if(id==R.id.cart){
+                replaceFragment(new CartFragment());
+            }
+            if(id==R.id.category){
+                replaceFragment(new CategoryFragment());
+            }
+            if(id==R.id.profile){
+                replaceFragment(new ProfileFragment());
+            }
+            return true;
+        });
     }
+
+    public void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction =fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout,fragment);
+        fragmentTransaction.commit();
+    }
+
 }

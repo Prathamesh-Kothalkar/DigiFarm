0a8b278b716b43f9ff7e9698a56ebeab148cf523
Prathamesh <prathameshkothalkar9021@gmail.com>
2024-01-12 16:33:55 +0530

    Initial commit

---
+++ app/src/main/java/com/example/digifarm/MainActivity.java
@@ -0,0 +1,15 @@
+
+package com.example.digifarm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

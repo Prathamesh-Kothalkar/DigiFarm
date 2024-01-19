f7f547487f1c90efcd9483a9897dad3435a27cbb
Prathamesh <prathameshkothalkar9021@gmail.com>
2024-01-19 21:42:59 +0530

    Firebase Connected

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -40,6 +41,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
     implementation("androidx.navigation:navigation-fragment:2.7.6")
     implementation("androidx.navigation:navigation-ui:2.7.6")
+    implementation("com.google.firebase:firebase-auth:22.3.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/digifarm/FarmerOtpActivity.java
+++ app/src/main/java/com/example/digifarm/FarmerOtpActivity.java
@@ -1,5 +1,6 @@
 package com.example.digifarm;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,10 +11,19 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.concurrent.TimeUnit;
 import java.util.regex.*;
 
 import com.chaos.view.PinView;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
 
 public class FarmerOtpActivity extends AppCompatActivity {
     Button login;
@@ -23,11 +33,15 @@ public class FarmerOtpActivity extends AppCompatActivity {
     PinView otp;
     TextInputEditText mobile;
 
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    long timeOutSecond=60L;
+    String verificationCode;
+    PhoneAuthProvider.ForceResendingToken resendingToken;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_farmer_otp);
-
          login=findViewById(R.id.farmer_login);
          next=findViewById(R.id.getOtp);
          resend=findViewById(R.id.sendAgain);
@@ -44,12 +58,14 @@ public class FarmerOtpActivity extends AppCompatActivity {
                Matcher match = regexMobile.matcher(phone);
                if(match.matches()){
                    otp.setEnabled(true);
-                   pb.setVisibility(View.VISIBLE);
+                    setInProgress(true);
                    login.setEnabled(true);
-                   showToast("Otp Genrated");
+                   showToast("Wait Verify");
+                   sendOtp(phone,false);
                }
                else{
                    showToast("Invalid Number");
+                   setInProgress(false);
                }
            }
        });
@@ -58,14 +74,78 @@ public class FarmerOtpActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 pb.setVisibility(View.GONE);
-                Intent in = new Intent(FarmerOtpActivity.this,FarmerLoginActivity.class);
-                startActivity(in);
-                finish();
+                String in_otp=otp.getText().toString();
+                PhoneAuthCredential credential =PhoneAuthProvider.getCredential(verificationCode,in_otp);
+                signIn(credential);
+                setInProgress(true);
             }
         });
     }
 
     void showToast(String msg){
         Toast.makeText(FarmerOtpActivity.this, msg, Toast.LENGTH_SHORT).show();
+    }
+
+    void sendOtp(String mobile,boolean isResend){
+        PhoneAuthOptions.Builder builder =PhoneAuthOptions.newBuilder(mAuth)
+                .setPhoneNumber(mobile)
+                .setTimeout(timeOutSecond, TimeUnit.SECONDS)
+                .setActivity(this)
+                .setCallbacks(new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                    @Override
+                    public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                        signIn(phoneAuthCredential);
+                        setInProgress(true);
+                    }
+
+                    @Override
+                    public void onVerificationFailed(@NonNull FirebaseException e) {
+                        showToast("Verification Failed");
+                        setInProgress(false);
+                    }
+
+                    @Override
+                    public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                        super.onCodeSent(s, forceResendingToken);
+                        verificationCode =s;
+                        resendingToken=forceResendingToken;
+                        showToast("Otp Sended");
+                        setInProgress(false);
+                    }
+                });
+        if(isResend){
+            PhoneAuthProvider.verifyPhoneNumber(builder.setForceResendingToken(resendingToken).build());
+        }
+        else{
+            PhoneAuthProvider.verifyPhoneNumber(builder.build());
+        }
+    }
+
+    void setInProgress(Boolean inProgress){
+        if(inProgress){
+            pb.setVisibility(View.VISIBLE);
+        }else{
+            pb.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    void signIn(PhoneAuthCredential phoneAuthCredential){
+        //login part
+        setInProgress(true);
+        mAuth.signInWithCredential(phoneAuthCredential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+               if(task.isSuccessful()){
+                   Intent in = new Intent(FarmerOtpActivity.this,FarmerLoginActivity.class);
+                   startActivity(in);
+                   finish();
+               }
+               else{
+                   showToast("Otp verification Failed");
+                   setInProgress(false);
+               }
+            }
+        });
+
     }
 }

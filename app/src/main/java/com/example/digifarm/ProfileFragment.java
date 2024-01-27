733e652818d508ea2997e3b7f66e69e05643b9cd
Prathamesh <prathameshkothalkar9021@gmail.com>
2024-01-27 22:07:48 +0530

    Fixes Problems

--- app/src/main/java/com/example/digifarm/ProfileFragment.java
+++ app/src/main/java/com/example/digifarm/ProfileFragment.java
@@ -3,47 +3,35 @@ package com.example.digifarm;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ProfileFragment extends Fragment {
 
     FirebaseAuth mAuth;
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    DatabaseReference databaseReference;
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
     public ProfileFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static ProfileFragment newInstance(String param1, String param2) {
         ProfileFragment fragment = new ProfileFragment();
         Bundle args = new Bundle();
@@ -56,10 +44,7 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
@@ -67,13 +52,35 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         mAuth=FirebaseAuth.getInstance();
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
         Button myOrderButton = view.findViewById(R.id.myOrderButton);
         Button myContactButton = view.findViewById(R.id.myContactButton);
         Button supportButton = view.findViewById(R.id.supportButton);
         Button tcButton = view.findViewById(R.id.tcButton);
         Button logoutButton = view.findViewById(R.id.logoutButton);
+        TextView name=view.findViewById(R.id.usernameTextView);
+        TextView phone=view.findViewById(R.id.phoneNumber);
+
+        databaseReference= FirebaseDatabase.getInstance().getReference("users/"+mAuth.getCurrentUser().getUid());
+
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()){
+                    User user = snapshot.getValue(User.class);
+                    name.setText(user.getName());
+                    phone.setText(user.getPhoneNumber());
+                }
+                else{
+                    Toast.makeText(getActivity(),"Error",Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
         myOrderButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
